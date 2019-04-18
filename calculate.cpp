195e07959022dca425efb615d6c5747822834908
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-19 00:56:37 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,1 +1,11 @@
+#include <string>
+#include "calculate.hpp"
+using namespace bullpgia;
 
+string calculateBullAndPgia(string a,string b){
+  string ans="";
+  int bul=0;
+  int pgia=0;
+  for(int i=0;i<a.length();i++){
+    if(a[i]==b[i])bool++;
+}
