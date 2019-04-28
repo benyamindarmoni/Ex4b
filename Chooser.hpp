8f47499c43f1e11d107b027a83d49c4270a3d111
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-28 21:30:28 +0300

    Update Chooser.hpp

--- Chooser.hpp
+++ Chooser.hpp
@@ -1,15 +1,13 @@
-    
+
 #pragma once
 using namespace std;
 #include <string>
 namespace bullpgia{
 class Chooser{
-string number;
+
     public:
-    Chooser(string);
-virtual std::string choose(uint length)=0;
-
-
+   
+virtual string choose(uint length)=0;
 
 };
 }
