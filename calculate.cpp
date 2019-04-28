8c988a413f91b591078fec6f1242cc0e36014b46
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-28 21:28:10 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -7,5 +7,12 @@ string calculateBullAndPgia(string a,string b){
   int bul=0;
   int pgia=0;
   for(int i=0;i<a.length();i++){
-    if(a[i]==b[i])bool++;
+      for(int j=0;j<a.length();i++){
+          if(a[i]==b[j]&&i!=j)pgia++; 
+          if(a[i]==b[j]&&i==j)bul++;
+      }
+   
+}
+ans=bul+" , "+pgia;
+
 }
