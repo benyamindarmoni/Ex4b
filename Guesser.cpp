1eaa5f177332d42265ca40682ea40138148e8f90
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-28 21:31:18 +0300

    Create Guesser.cpp

---
+++ Guesser.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <string>
+#include "Guesser.hpp"
+using  namespace bullpgia;
+ Guesser::Guesser(string s){
+     number=s;
+ }
+ void startNewGame(uint a){
+     
+ }
+ void learn(string s){
+     
+ }
