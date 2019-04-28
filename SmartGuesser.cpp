cd5844c09366018f7f63c2233e6da3a420007915
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-28 21:32:08 +0300

    Update and rename SmartGuesser to SmartGuesser.cpp

---
+++ SmartGuesser.cpp
@@ -0,0 +1,18 @@
+
+#include "SmartGuesser.hpp"
+#include <stdlib.h>
+#include <string>
+using namespace std;
+namespace bullpgia{
+ string SmartGuesser::guess() {
+   
+     return "";
+ }
+ void SmartGuesser::startNewGame(uint a) {
+     
+ }
+ void SmartGuesser::learn(string a) {
+     
+ }
+ 
+}
