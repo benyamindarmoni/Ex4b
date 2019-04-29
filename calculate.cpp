024eaad5367cec9707a7f7143fa327640f6b7905
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-29 15:01:05 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -5,19 +5,31 @@
 using namespace std;
 
 
-
  string bullpgia::  calculateBullAndPgia( string a, string b){
   int bul=0;
   int pgia=0;
   for(int i=0;i<a.length();i++){
-      for(int j=0;j<a.length();j++){
-          if(a[i]==b[j]&&i!=j)pgia++; 
-          if(a[i]==b[j]&&i==j)bul++;
+     
+          if(a[i]==b[i]){
+              bul++;
+              b[i]=a[i]='x';
       }
    
+}
+if(bul!=a.length()){
+  for(int i=0;i<a.length();i++){
+      for(int j=0;j<a.length();j++){
+          if(a[i]!='x'&&a[i]==b[j]){
+              pgia++; 
+         b[j]='x';
+         continue;
+      }
+}
+}
 }
 string ans=to_string(bul);
    ans+=",";
    ans+=to_string(pgia);
 return ans;
-}
+     
+ }
