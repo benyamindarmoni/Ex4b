8c551cb28d039103a56aef1e7ceb4f1ef5e07337
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-29 12:24:14 +0300

    Update Guesser.cpp

--- Guesser.cpp
+++ Guesser.cpp
@@ -2,12 +2,7 @@ using namespace std;
 #include <string>
 #include "Guesser.hpp"
 using  namespace bullpgia;
- Guesser::Guesser(string s){
-     number=s;
- }
- void startNewGame(uint a){
-     
- }
- void learn(string s){
-     
- }
+
+
+
+ 
