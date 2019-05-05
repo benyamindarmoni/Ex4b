ba7f7dc4945beb5ab2feb988eaa3ea62031e2bd0
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-05-05 13:39:19 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,15 +1,21 @@
 #pragma once
-#include "calculate.hpp"
+
 #include "Guesser.hpp"
 #include <iostream>
-
+#include "calculate.hpp"
 using namespace std;
 namespace bullpgia{
 
     class SmartGuesser : public bullpgia:: Guesser{
     public:
-        string number;
-        uint len;
+        string curr_guess;
+        bool*  arr;
+        uint array_len;
+        uint number_len;
+        ~SmartGuesser() {
+         delete [] arr;
+        }
+
         
         string guess() override;
         void startNewGame(uint)override ;
